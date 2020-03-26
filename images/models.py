beb054057b96ecf018ebff47a89abd08204253bc
Kyle Bruder <kylebruder@pm.me>
2020-03-25 20:54:57 -0700

    add thumbnail function with post_save signal for ImageModel

--- images/models.py
+++ images/models.py
@@ -1,25 +1,36 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from tags.models import Tag
+#from marshmallows import Marshmallow
 # Create your models here.
 
 class Image(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     creation_date = models.DateTimeField(default=timezone.now)
     image_file = models.ImageField(upload_to='images/%Y/%m/%d/')
+    thumbnail_file = models.ImageField(upload_to='images/thumbnails/%Y/%m/%d/', blank=True)
     caption = models.CharField(max_length=256, default="a picture is worth a thousand words.")
     alt_text = models.CharField(max_length=64, default="a compelling image")
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return self.alt_text
 
     def get_absolute_url(self):
         return reverse('images:image_detail', kwargs={'pk': self.pk})
+   
+    def add_thumbnail(self):
+        image_ready.send(sender=self.__class__)
 
 class Gallery(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    creation_date = models.DateTimeField(default=timezone.now)
     title = models.CharField(max_length=256, default="Untitled")
     pictures = models.ManyToManyField(Image)
+    caption = models.CharField(max_length=256, default="a curated collection of imagery")
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
---
+++ images/signals.py
@@ -0,0 +1,35 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from pathlib import Path
+from PIL import Image as img
+from images.models import Image
+
+@receiver(post_save, sender=Image)
+def add_thumbnail(sender, instance, created, **kwargs):
+    '''
+    Add a thumbnail image to the Image model after it is saved to DB
+    '''
+    if created:
+        upload_dir = instance.creation_date.strftime(
+            'media/images/%Y/%m/%d/'
+        )
+        p = Path('media')
+        q = p / instance.image_file.name
+        image = img.open(q)
+        max_size = (200, 200)
+        image.thumbnail(max_size)
+        thumb_dir = instance.creation_date.strftime(
+            'images/thumbnails/%Y/%m/%d/'
+        )
+        file_name = instance.image_file.name.split('/')[-1].split('.')[0]
+        extension = instance.image_file.name.split('/')[-1].split('.')[-1]
+        thumb_file_name = '{}{}{}'.format(file_name, '-thumbnail.', extension)
+        p = Path('media')
+        q = p / thumb_dir
+        Path.mkdir(q, parents=True, exist_ok=True)
+        image.save(q / thumb_file_name)
+        p = Path(thumb_dir)
+        q = p / thumb_file_name
+        instance.thumbnail_file = str(q)
+        instance.save()
+
